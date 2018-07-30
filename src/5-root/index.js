59046b5c8fb9b2c05dd19a434b5ff192d34b8518
minooo <552594996@qq.com>
2018-07-30 14:03:48 +0800

    base

--- src/5-root/index.js
+++ src/5-root/index.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import DevTools from "mobx-react-devtools";
+// import DevTools from "mobx-react-devtools";
 import Boot from "./Boot";
 import routes from "./routes";
 
@@ -7,7 +7,6 @@ class Root extends Component {
   render() {
     return (
       <Boot>
-        <DevTools />
         <Fragment>{routes}</Fragment>
       </Boot>
     );
